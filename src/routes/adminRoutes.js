0d671c191bb8e031f208cf05ae3d87ca942a91c1
pragya chhabra <pragyachhabbra@gmail.com>
2019-10-17 13:26:28 +0200

    Add files via upload

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
+
+const adminRouter = express.Router();
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    bookId: 656,
+    read: false
+  },
+  {
+    title: 'Les Misérables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    bookId: 24280,
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    bookId: 242,
+    read: false
+  },
+  {
+    title: 'A Journey into the Center of the Earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    bookId: 2,
+    read: false
+  },
+  {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    bookId: 87,
+    read: false
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    bookId: 20,
+    read: false
+  },
+  {
+    title: 'Life On The Mississippi',
+    genre: 'History',
+    author: 'Mark Twain',
+    bookId: 76,
+    read: false
+  },
+  {
+    title: 'Childhood',
+    genre: 'Biography',
+    author: 'Lev Nikolayevich Tolstoy',
+    bookId: 86,
+    read: false
+  }];
+
+function router(nav) {
+  adminRouter.route('/')
+    .get((req, res) => {
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+
+          const db = client.db(dbName);
+
+          const response = await db.collection('books').insertMany(books);
+          res.json(response);
+        } catch (err) {
+          debug(err.stack);
+        }
+
+        client.close();
+      }());
+    });
+  return adminRouter;
+}
+
+module.exports = router;
